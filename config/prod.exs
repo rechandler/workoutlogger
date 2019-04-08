4e4dfa7f45eb29a209a9b1447d80535565deb97c
ryann <ryann.chandler@vitalsource.com>
2019-04-07 20:23:24 -0500

    building updates

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :workout_app, WorkoutAppWeb.Endpoint,
   http: [:inet6, port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 80],
+  url: [host: "${APP_NAME}.gigalixirapp.com", port: 443],
   cache_static_manifest: "priv/static/cache_manifest.json",
   server: true,
   secret_key_base: "${SECRET_KEY_BASE}"
@@ -25,7 +25,7 @@ config :workout_app, WorkoutApp.Repo,
        url: "${DATABASE_URL}",
        database: "",
        ssl: true,
-       pool_size: 1
+       pool_size: 2
 
 # ## SSL Support
 #
